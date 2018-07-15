77bf033ded1ebb844839ff87b25f01fa524b89a6
DuckGrand <wangqi_duck@163.com>
2018-07-15 13:43:18 +0800

    activiy_life修改

--- app/src/main/java/com/dream/duck/anewtry/MainActivity.java
+++ app/src/main/java/com/dream/duck/anewtry/MainActivity.java
@@ -2,6 +2,7 @@ package com.dream.duck.anewtry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("activity_main","onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("activity_main","onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("activity_main","onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("activity_main","onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("activity_main","onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("activity_main","onPause()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("activity_main","onDestroy()");
     }
 }
